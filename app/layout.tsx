51432b3055e4299892d0933d9ca7fd6c4a8c5255
kmishmael <kmishmael@gmail.com>
2024-05-29 22:24:12 +0300

    chore: change font to Raleway

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Raleway } from "next/font/google";
 import "./globals.css";
 import { BASE_URL } from "@/global.config";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Raleway({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   metadataBase: new URL(BASE_URL),
