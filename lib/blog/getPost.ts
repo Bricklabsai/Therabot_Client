c47f40aa6a4676c0b21c681fa90daf412f489680
kmishmael <kmishmael@gmail.com>
2024-06-27 17:42:48 +0300

    fix: add fallback

--- lib/blog/getPost.ts
+++ lib/blog/getPost.ts
@@ -2,13 +2,14 @@ import axios from "axios";
 
 import { StrapiBase, PostSummary, Post } from ".";
 
-const STRAPI_URL = process.env.STRAPI_URL;
-const STRAPI_AUTH_TOKEN = process.env.STRAPI_AUTH_TOKEN;
+const STRAPI_URL = process.env.STRAPI_URL || null;
+const STRAPI_AUTH_TOKEN = process.env.STRAPI_AUTH_TOKEN || null;
 
 
 export const getPosts = async () => {
-    if (STRAPI_URL === undefined) {
-        throw Error("STRAPI_URL not defined.")
+    if (STRAPI_URL === null) {
+        Error("STRAPI_URL not defined.")
+        return null
     }
     try {
         const res = await axios.get<StrapiBase<PostSummary[]>>(`${STRAPI_URL}/api/posts?fields[0]=title&fields[1]=excerpt&fields[2]=slug&fields[3]=updatedAt&populate[0]=featuredImage&populate[1]=author&populate[author][populate]=image`, {
@@ -25,8 +26,9 @@ export const getPosts = async () => {
 
 
 export const getPost = async (slug: string) => {
-    if (STRAPI_URL === undefined) {
-        throw Error("STRAPI_URL not defined.")
+    if (STRAPI_URL === null) {
+        Error("STRAPI_URL not defined.")
+        return null
     }
     try {
         const res = await axios.get<StrapiBase<Post>>(`${STRAPI_URL}/api/posts/${slug}`,
